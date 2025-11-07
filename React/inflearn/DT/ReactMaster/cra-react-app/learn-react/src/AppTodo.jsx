93164c5c95a6f4f35e47195a85b9d97cfaa63332
Juyong <ht9943@naver.com>
2025-11-07 19:13:28 +0900

    스벅에서

--- React/inflearn/DT/ReactMaster/cra-react-app/learn-react/src/AppTodo.jsx
+++ React/inflearn/DT/ReactMaster/cra-react-app/learn-react/src/AppTodo.jsx
@@ -4,14 +4,61 @@ import TodoList from './components/todo/TodoList';
 
 
 function AppTodo(props) {
+  const [todoText, setTodoText] = useState('');
+
   const [todos, setTodos] = useState([
-    {id:0, label:"HTML&CSS 공부하기"},
-    {id:1, label:"자바스크립트 공부하기"}
-  ])
+    {id:0, text:"HTML&CSS 공부하기", done:false},
+    {id:1, text:"자바스크립트 공부하기", done:false}
+  ]);
+  const handleTodoTextChange = (e) => {
+      setTodoText(e.target.value);
+  }
+  //꼭! 새 배열 복사해서 넣기
+  const handleAddTodo = () =>{
+    const nextId = todos.length;
+    setTodos([
+      ...todos,
+      {id : nextId, text: todoText}
+    ])
+    //여기서 input 내에 있는 값을 빈값으로 만들어주면 된다.
+    setTodoText('');
+  }
+  //기능을 자식 컴포넌트에서 만들 생각하지말고
+  //부모 컴포넌트에서 props로 기능을 내려주면 된다.
+  //삭제 기능
+  const handleDeleteTodo = (deleteId) =>{
+    //filter 문법 확인!
+    //해당하는 id의 값을 빼는거라고 생각하지말고
+    //해당하는 id의 값만 빼고 반환하기
+    const newTodos = todos.filter(item => item.id !== deleteId);
+    setTodos(newTodos);
+  }
+  //input에 포커스 되어있을때 enter키 누르면 추가되는 기능
+  const handleKeyDown = (e) =>{
+    if(e.key === "Enter"){
+      handleAddTodo();
+    }
+  }
+  //done여부를 true/false 바꾸는 기능
+  const handleToggleTodo = (id, done) =>{
+    //기존 배열 안의 객체 속성을 변경!
+    //여기부터다 !!!!! 17분30초!!! todos.map( => )
+
+  }
   return (
     <div>
       <h2>할일목록</h2>
-      <TodoList todos={todos}/>
+      <input 
+        type="text" 
+        value={todoText} 
+        onChange={handleTodoTextChange} 
+        onKeyDown={handleKeyDown}></input>
+      <button onClick={handleAddTodo}>추가</button>
+      <TodoList 
+      todos={todos}
+      onDeleteTodo={handleDeleteTodo}
+      onToggleTodo={handleToggleTodo}
+      />
     </div>
   );
 }
--- React/inflearn/DT/ReactMaster/cra-react-app/learn-react/src/components/todo/TodoList.jsx
+++ React/inflearn/DT/ReactMaster/cra-react-app/learn-react/src/components/todo/TodoList.jsx
@@ -1,10 +1,19 @@
-function TodoList({todos = []}){
-  const items = [...todos];
-  items.push({id : 2, label:"포트폴리오 사이트 만들기"});
+function TodoList({todos = [], onDeleteTodo, onToggleTodo}){
+
   return(
     <ul>
-      {items.map(item =>(
-        <li key={item.id}>{item.label}</li>
+      {todos.map(item =>(
+        <li key={item.id}>
+          <input 
+            type="checkbox"
+            checked={item.done}
+            onChange={(e)=> onToggleTodo(item.id, e.target.checked)}
+          />
+          <span>{item.text}</span>
+          <button
+            onClick={()=> onDeleteTodo(item.id)}
+          >X</button>
+        </li>
       ))}
     </ul>
   )
